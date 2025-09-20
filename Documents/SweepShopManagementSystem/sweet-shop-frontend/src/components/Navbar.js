3f1331ce2537c9db0661f5a2f66adb77d8d1e287
JarpalaSony <thisissonycse190@gmail.com>
2025-09-20 10:09:53 +0530

    added navbar mobile menu

--- Documents/SweepShopManagementSystem/sweet-shop-frontend/src/components/Navbar.js
+++ Documents/SweepShopManagementSystem/sweet-shop-frontend/src/components/Navbar.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState,useEffect,useContext } from "react";
+import { getCurrentUser } from "../services/authService";
+import { Link,useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import {logout} from "../services/authService";
 import "../styles/Navbar.css";
 
 export default function Navbar() 
 {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
-
+  const {user,setUser} = useContext(AuthContext);
+  const auth=useContext(AuthContext)
+  console.log("AuthContext value in Navbar:",auth);
+  const navigate=useNavigate();
+const handleSignOut = () => {
+    logout() // remove JWT token
+    setUser(null);                     // reset user state
+    navigate("/login")                    // redirect to home
+  };
   return (
     <nav className="sweet-navbar">
       <div className="sweet-container">
-        <Link to="/" className="sweet-logo">
-          🍭 SweetShop
-        </Link>
+        <Link to="/" className="sweet-logo">🍭 SweetShop</Link>
         <div className="hamburger" onClick={toggleMenu}>
           <span className={`bar ${isOpen ? "open" : ""}`}></span>
           <span className={`bar ${isOpen ? "open" : ""}`}></span>
@@ -24,13 +33,28 @@ export default function Navbar()
           <li><Link to="/orders">Orders</Link></li>
           <li><Link to="/customers">Customers</Link></li>
           <li className="mobile-login">
-            <Link to="/login" className="login-btn">Login</Link>
+            {!user ? (
+              <>
+                <Link to="/register" className="login-btn">Register</Link>
+                <Link to="/login" className="login-btn">Login</Link>
+              </>
+            ) : (
+              <span>Welcome, {user.name}</span>
+            )}
           </li>
         </ul>
         <div className="desktop-login">
-          <Link to="/login" className="login-btn">Login</Link>
+          {!user ? (
+            <>
+              <Link to="/register" className="login-btn">Register</Link>
+              <Link to="/login" className="login-btn">Login</Link>
+            </>
+          ) : (<>
+            <span>Welcome, {user.name}</span>
+            <button onClick={handleSignOut} className="logout-btn">Sign Out</button>
+            </>
+          )}
         </div>
-
       </div>
     </nav>
   );
