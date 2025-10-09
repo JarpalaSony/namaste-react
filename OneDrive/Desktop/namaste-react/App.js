99c0c379bf2f244adcf17c2faca6b66fe6d5259c
JarpalaSony <thisissonycse190@gmail.com>
2025-10-09 16:22:39 +0530

    changes in App.js,index.html package.json is saved

--- OneDrive/Desktop/namaste-react/App.js
+++ OneDrive/Desktop/namaste-react/App.js
@@ -1,12 +1,21 @@
-let parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"Heading 1"),
-        React.createElement("h2",{},"Heading 2")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"Heading 1"),
-        React.createElement("h2",{},"Heading 2")
-    ])
-]);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css"
+import { FaUserCircle } from 'react-icons/fa';
+const Header= ()=>(
+    <header className="header">
+        <div className="logo">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYt3pWcX4oGm1F8n1kKXl0b2r7b5k2kU8J4g&usqp=CAU" alt="logo" width="75px"/>
+            <h2>React App</h2>
+        </div>
+        <div className="search-bar">
+            <input type="text" placeholder="Search..."/>
+            <button>Search🔍</button>
+        </div>
+        <div className="user-icon">
+        <FaUserCircle size={30} />
+      </div>
+    </header>
+)
+const root =ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Header/>)
